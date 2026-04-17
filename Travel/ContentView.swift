862314f1d89e0beddfb2e60c0219aeac9b621982
akuayip <muhammadarief1234516@gmail.com>
2026-04-17 13:32:56 +0700

    Initial Commit

---
+++ Travel/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Travel
+//
+//  Created by M. Arief Rahman Hakim on 17/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
