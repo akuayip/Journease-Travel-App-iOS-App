d387c4b724814a0ea7758995f3ab429ef775ce6a
akuayip <muhammadarief1234516@gmail.com>
2026-04-23 09:38:30 +0700

    feat: add DocumentCategory and DocumentSearchView components

---
+++ Core/Models/DocumentCategory.swift
@@ -0,0 +1,18 @@
+//
+//  DocumentCategory.swift
+//  Journease
+//
+//  Created by M. Arief Rahman Hakim on 21/04/26.
+//
+
+import SwiftUI
+
+struct DocumentCategory: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DocumentCategory()
+}
---
+++ Features/Document/DocumentSearchView.swift
@@ -0,0 +1,18 @@
+//
+//  DocumentSearchView.swift
+//  Journease
+//
+//  Created by M. Arief Rahman Hakim on 22/04/26.
+//
+
+import SwiftUI
+
+struct DocumentSearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DocumentSearchView()
+}
