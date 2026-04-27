1f8c656e47d2b192eb543197803a4963285b87ff
akuayip <muhammadarief1234516@gmail.com>
2026-04-28 00:02:40 +0700

    feat: Update asset filenames and remove unused image sets; add FileManagerHelper view

---
+++ Core/Helpers/FileManagerHelper.swift
@@ -0,0 +1,18 @@
+//
+//  FileManagerHelper.swift
+//  Journease
+//
+//  Created by M. Arief Rahman Hakim on 27/04/26.
+//
+
+import SwiftUI
+
+struct FileManagerHelper: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FileManagerHelper()
+}
